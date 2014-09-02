f1b56ec1c2b2887f92720e4146fc0603a440bf92
Adam Carter <adamrcarter@gmail.com>
2014-09-02 11:00:20 -0500

    continued work on the JS practice problems

--- 4_simple_data_manipulation/1.js
+++ 4_simple_data_manipulation/1.js
@@ -7,3 +7,42 @@
 // ==
 // <, >, <=, >=
 
+// adds two numbers
+var num1 = 1
+var num2 = 2
+console.log(num1 + num2);
+
+// subtracts two numbers
+var num1 = 5
+var num2 = 2
+console.log(num1 - num2);
+
+// divides two numbers
+var num1 = 6
+var num2 = 2
+console.log(num1 / num2);
+
+// multiplies two numbers
+var num1 = 1
+var num2 = 2
+console.log(num1 * num2);
+
+// returns the remainder
+var num1 = 7
+var num2 = 3
+console.log(num1 % num2);
+
+// determines if two variables are equal or not
+var num1 = 1
+var num2 = 2
+console.log(num1 == num2);
+
+// checks for greater than
+var num1 = 1
+var num2 = 2
+console.log(num1 > num2);
+
+// checks to see if a variable is less than or equal to another variable
+var num1 = 1
+var num2 = 2
+console.log(num1 <= num2);
---
+++ 4_simple_data_manipulation/2.js
@@ -0,0 +1,1 @@
+// Write a program that sets each of two variables to a number, then displays the integer quotient and integer result to the screen, like so: 7 / 3 = 2 r 1. (Hint: parseInt() in JavaScript is like to_i in Ruby.)
